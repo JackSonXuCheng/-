39a2ba173a528fb6684bf258cf1a35a1bfdc9336
xuchengxu <1301506639@qq.com>
2021-01-25 22:12:39 +0800

    navigation tarbar

--- miniprogram-day1-helloworld/pages/base/base.js
+++ miniprogram-day1-helloworld/pages/base/base.js
@@ -5,9 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: '小明',
+    age: 18,
+    hobbys:['篮球','羽毛球','兵兵球'],
+    result:0,
 
   },
-
+  addButtong(){
+    //数据渲染 
+    this.setData({
+      result: this.data.result+=1
+    })
+  },
+  subButtong(){
+    //数据渲染 
+    this.setData({
+      result: this.data.result-=1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
